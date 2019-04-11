7ff20d5c10601c4b1cd13a30c19c98e25870bf9b
Ponomarev Vyacheslav <vaponomarev@edu.hse.ru>
2019-04-11 13:04:08 +0300

    Added functions to check f1/em scores

--- src/model/utils.py
+++ src/model/utils.py
@@ -1,3 +1,9 @@
+import argparse
+import re
+import string
+from collections import Counter
+
+
 def str2bool(v):
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
         return True
@@ -6,7 +12,8 @@ def str2bool(v):
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
 
-class AverageMeter():
+
+class AverageMeter:
     """Keep exponential weighted averages."""
 
     def __init__(self, beta=0.99):
@@ -27,3 +34,60 @@ class AverageMeter():
         self.moment = self.beta * self.moment + (1 - self.beta) * val
         # bias correction
         self.value = self.moment / (1 - self.beta ** self.t)
+
+
+def _normalize_answer(s):
+    def remove_articles(text):
+        return re.sub(r'\b(a|an|the)\b', ' ', text)
+
+    def white_space_fix(text):
+        return ' '.join(text.split())
+
+    def remove_punc(text):
+        exclude = set(string.punctuation)
+        return ''.join(ch for ch in text if ch not in exclude)
+
+    def lower(text):
+        return text.lower()
+
+    return white_space_fix(remove_articles(remove_punc(lower(s))))
+
+
+def _exact_match(pred, answers):
+    if pred is None or answers is None:
+        return False
+    pred = _normalize_answer(pred)
+    for a in answers:
+        if pred == _normalize_answer(a):
+            return True
+    return False
+
+
+def _f1_score(pred, answers):
+    def _score(g_tokens, a_tokens):
+        common = Counter(g_tokens) & Counter(a_tokens)
+        num_same = sum(common.values())
+        if num_same == 0:
+            return 0
+        precision = 1. * num_same / len(g_tokens)
+        recall = 1. * num_same / len(a_tokens)
+        f1 = (2 * precision * recall) / (precision + recall)
+        return f1
+
+    if pred is None or answers is None:
+        return 0
+    g_tokens = _normalize_answer(pred).split()
+    scores = [_score(g_tokens, _normalize_answer(a).split()) for a in answers]
+    return max(scores)
+
+
+def score(pred, truth):
+    assert len(pred) == len(truth)
+    f1 = em = total = 0
+    for p, t in zip(pred, truth):
+        total += 1
+        em += _exact_match(p, t)
+        f1 += _f1_score(p, t)
+    em = 100. * em / total
+    f1 = 100. * f1 / total
+    return em, f1
