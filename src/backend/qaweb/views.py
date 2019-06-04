3915c882fb9cc6291d9668e0439b840ef079b2c9
Movses Elbakian <mvrsel@yandex-team.ru>
2019-06-04 17:51:34 +0300

    finally

--- src/backend/qaweb/views.py
+++ src/backend/qaweb/views.py
@@ -6,7 +6,7 @@ from typing import Dict
 import ujson
 from aiohttp import web
 
-from qaweb.connectors import SaaSConnector, NetConnector
+from qaweb.connectors import SaaSConnector, NetConnector, RedisConnector
 from qaweb.custom_prepro import CustomPrepro
 
 logger = logging.getLogger(__name__)
@@ -19,33 +19,43 @@ async def search_handler(request: web.Request) -> web.Response:
     except ValueError:
         raise web.HTTPBadRequest(text="JSON is malformed")
 
+    redis: RedisConnector = request.app['redis']
+    await redis.connect()
     saas: SaaSConnector = request.app['saas']
+
     if request_body.get('text'):
         texts = [request_body['text']]
         title = [""]
     else:
         st = time.time()
-        documents = await saas.get_documents(query)
+        title = await saas.get_documents(query)
+        print(title, flush=True, file=sys.stdout)
         print('Time in Google', time.time() - st, flush=True, file=sys.stdout)
 
         st = time.time()
         texts = []
-        title = []
-        for doc in documents:
+        for t in title:
             try:
-                text = await doc.summary()
+                text = await redis.get_text(t)
+                if not text:
+                    text = await saas.get_wikipage(t).summary()
+                    await redis.dump_text(t, text)
                 if len(text) > 1:
                     texts.append(text)
-                    title.append(doc.title)
-            except Exception:
-                pass
+            except Exception as e:
+                print(e)
         print('Time in wikipedia', time.time() - st, flush=True, file=sys.stdout)
 
     st = time.time()
     prepro: CustomPrepro = request.app['prepro']
     preprocessed_data = []
-    for text in texts:
-        preprocessed_data.append(prepro.prepro(text, query))
+    for i, text in enumerate(texts):
+        prepro_text = await redis.get_preprocessed(title[i])
+        if not prepro_text:
+            prepro_text = prepro.prepro_text(text)
+            await redis.dump_text(title[i], text)
+        prepro_query = prepro.prepro_text(query)
+        preprocessed_data.append(prepro.prepro_crossed(prepro_text, prepro_query))
     print('Time in CustomPrepro', time.time() - st, flush=True, file=sys.stdout)
 
     st = time.time()
